8e92a0a11529429f17bf9f9330084e5df8215e73
FRAMGIA\tran.xuan.dat <tran.xuan.dat@framgia.com>
2019-02-13 09:56:52 +0700

    [Add] Entity API Wrapper File

---
+++ src/Uiza.Net/Services/EntityService.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Uiza.Net.Parameters;
+using Uiza.Net.Response;
+using Uiza.Net.Services.Interface;
+
+namespace Uiza.Net.Services
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class EntityService : Service,
+        ICreate<UizaData, BaseParameter>,
+        IRetrieve<UizaData, BaseParameter>,
+        IUpdate<UizaData, BaseParameter>,
+        IDelete<UizaData, BaseParameter>,
+        IRetrieveList<UizaData, BaseParameter>,
+        IEntityService
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public UizaData Create(BaseParameter param)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public UizaData Delete(BaseParameter param)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public UizaData GetAWSUploadKey()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public UizaData GetStatusPublish()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public UizaData List(BaseParameter param)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public UizaData Publish()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public UizaData Retrieve(BaseParameter param)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public UizaData Update(BaseParameter param)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
