726306eaedc16f2783fa97b035fd95014a484d1a
FRAMGIA\tran.xuan.dat <tran.xuan.dat@framgia.com>
2019-02-13 13:21:56 +0700

    [Add] Create Entity Service

---
+++ src/Uiza.Net/Enum/EntityInputTypes.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace Uiza.Net.Enum
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum EntityInputTypes
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [EnumMember(Value = "http")]
+        Http,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [EnumMember(Value = "s3")]
+        S3,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [EnumMember(Value = "ftp")]
+        Ftp,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [EnumMember(Value = "s3-uiza")]
+        S3Uiza
+    }
+}
---
+++ src/Uiza.Net/Parameters/Entity/CreateEntity.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using Uiza.Net.Enum;
+using Uiza.Net.Response;
+
+namespace Uiza.Net.Parameters
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CreateEntityParameter : BaseParameter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("url")]
+        public string URL { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("inputType")]
+        public EntityInputTypes InputType { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("metadataId")]
+        public string MetadataId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("shortDescription")]
+        public string ShortDescription { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("poster")]
+        public string Poster { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("thumbnail")]
+        public string Thumbnail { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("metadataIds")]
+        public List<string> MetadataIds { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("embedMetadata")]
+        public EmbedData EmbedMetadata { get; set; }
+    }
+}
