e8d433bc1bb97add5b2044b126ba5ee8cd1a2872
FRAMGIA\tran.xuan.dat <tran.xuan.dat@framgia.com>
2019-02-14 11:58:49 +0700

    [Create] Create XUnit Uiza Dot Net

---
+++ src/UizaTest/Base/UizaTestBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UizaTest
+{
+    [Collection("uiza-mock-test-collection")]
+    public class UizaTestBase
+    {
+    }
+}
---
+++ src/UizaTest/UizaMockTestCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UizaTest
+{
+    [CollectionDefinition("uiza-mock-test-collection")]
+    public class UizaMockTestCollection :
+          ICollectionFixture<InitConfigTest>
+    {
+    }
+}
