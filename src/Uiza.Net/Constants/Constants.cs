63ec10f80464c53f2c18a6c90b3959ae6e360d27
FRAMGIA\tran.xuan.dat <tran.xuan.dat@framgia.com>
2019-03-12 15:03:07 +0700

    [Create] Create Format URL Function

--- src/Uiza.Net/Constants/Constants.cs
+++ src/Uiza.Net/Constants/Constants.cs
@@ -6,6 +6,6 @@
     internal static partial class Constants
     {
         internal static string DEFAULT_API_ROUTE => "api/public/v4";
-        internal static string DEFAULT_DOMAIN => "http://dev-ap-southeast-1-api.uizadev.io/";
+        internal static string DEFAULT_DOMAIN => "https://stag-ap-southeast-1-api.uizadev.io/";
     }
 }
